0c828c780a84837f15fc8107b3fee7c28cd1e8e8
Divya B Gowda <117468949+DivyaBGowda484@users.noreply.github.com>
2025-07-31 12:43:26 +0530

    Stuctured

--- app/services/notification_service.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import Session
-from app.models.notification_model import Notification
-
-class NotificationService:
-    @staticmethod
-    def get_all(db: Session):
-        return db.query(Notification).all()
